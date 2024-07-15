b91c9ff90360f817f3004978e82b88bb0370c741
Aitor Garcia Fernandez <aitorgarciafernandezcv@gmail.com>
2024-07-15 15:05:00 +0200

    test fixed and done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function arrSum(arr1, arr2) {
+export function arrSum(arr1, arr2) {
   const sumArr1 = arr1.reduce((acc, curr) => acc + curr, 0);
   const sumArr2 = arr2.reduce((acc, curr) => acc + curr, 0);
   return sumArr1 + sumArr2;
